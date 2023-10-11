1b04f9f53f3a3b9cd3556cb077b2b38238292678
dml2609 <manhlinh2k1er@gmail.com>
2023-10-11 08:39:31 +0700

    init project

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+import java.util.Arrays;
+public class HelloWorld {
+    public static void main(String[] args) {
+        Scanner input= new Scanner(System.in);
+        System.out.print("What is your name? ");
+        String name= input.nextLine();
+        System.out.print("How old are you? ");
+        int age = input.nextInt();
+        }
+    }
+}
